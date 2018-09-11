6eb1e9e53c0ebe93f5ffb6d4e7de5b7ba5987bed
Gayan Belpamulle <gayan.belpamulle@digio.com.au>
2018-09-11 10:37:39 +1000

    Add the first /hello functional enpoint with QuoteRouter and QuoteHandler

--- stockmarket/src/main/java/demo/spring/webflux/stockmarket/service/QuoteHandler.java
+++ stockmarket/src/main/java/demo/spring/webflux/stockmarket/service/QuoteHandler.java
@@ -1,7 +1,22 @@
 package demo.spring.webflux.stockmarket.service;
 
+import reactor.core.publisher.Mono;
+
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
 
 @Component
 public class QuoteHandler {
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ok().contentType(TEXT_PLAIN)
+                .body(BodyInserters.fromObject("Hello Spring!"));
+    }
+
 }
