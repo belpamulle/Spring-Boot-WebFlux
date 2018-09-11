f93875b1d80aaf54606d9ebd12eba3bf75726b44
Gayan Belpamulle <gayan.belpamulle@digio.com.au>
2018-09-11 14:08:44 +1000

    Use the WebClient to stream JSON to the browser

---
+++ trading-service/src/main/java/demo/spring/webflux/tradingservice/controller/QuotesController.java
@@ -0,0 +1,32 @@
+package demo.spring.webflux.tradingservice.controller;
+
+import demo.spring.webflux.tradingservice.model.Quote;
+import reactor.core.publisher.Flux;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
+
+@Controller
+public class QuotesController {
+    @GetMapping("/quotes")
+    public String quotes() {
+        return "quotes";
+    }
+
+    @GetMapping(path = "/quotes/feed", produces = TEXT_EVENT_STREAM_VALUE)
+    @ResponseBody
+    public Flux<Quote> quotesStream() {
+        return WebClient.create("http://localhost:8081")
+                .get()
+                .uri("/quotes")
+                .accept(APPLICATION_STREAM_JSON)
+                .retrieve()
+                .bodyToFlux(Quote.class)
+                .share()
+                .log("io.spring.workshop.tradingservice");
+    }
+}
